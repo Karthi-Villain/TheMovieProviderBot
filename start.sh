9e2c682876c0bbfb1885f0e7fb1f7d2b60b3836b
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2024-04-17 11:57:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git TheMovieProviderBot
+  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO TheMovieProviderBot
+  git clone $UPSTREAM_REPO 
 fi
 ls
-cd TheMovieProviderBot
+#cd TheMovieProviderBot
 ls
 pip3 install -U -r requirements.txt
 echo "Starting TheMovieProviderBot...."
